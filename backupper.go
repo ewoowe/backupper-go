c132cd21f0f1fe629e157658e538a04b0daf8219
ewoowe <1394166475@qq.com>
2023-07-25 00:16:34 +0800

    remove debug info

--- backupper.go
+++ backupper.go
@@ -1,7 +1,6 @@
 package backupper_go
 
 import (
-	"log"
 	"runtime"
 	"strconv"
 	"strings"
@@ -10,7 +9,7 @@ import (
 
 type Backupper struct {
 	state int32
-	Run func()
+	Run   func()
 }
 
 const (
@@ -19,44 +18,30 @@ const (
 	RunningBackup = 2
 )
 
-func (b *Backupper) DoRun()  {
-	log.Printf("%d enter 0", getGoId())
+func (b *Backupper) DoRun() {
 	for {
 		if b.state == NoRunning {
-			log.Printf("%d enter 1", getGoId())
 			if atomic.CompareAndSwapInt32(&b.state, NoRunning, Running) {
-				log.Printf("%d enter 2", getGoId())
 				b.Run()
 				for {
 					if b.state == RunningBackup {
-						log.Printf("%d enter 3", getGoId())
 						atomic.StoreInt32(&b.state, Running)
 						b.Run()
 					} else {
-						log.Printf("%d enter 4", getGoId())
 						break
 					}
 				}
-				log.Printf("%d enter 5", getGoId())
 				if atomic.CompareAndSwapInt32(&b.state, Running, NoRunning) {
-					log.Printf("%d enter 6", getGoId())
 					break
 				}
-				log.Printf("%d enter 7", getGoId())
 				atomic.StoreInt32(&b.state, NoRunning)
 			}
 		} else if b.state == Running {
-			log.Printf("%d enter 8", getGoId())
 			if atomic.CompareAndSwapInt32(&b.state, Running, RunningBackup) {
-				log.Printf("%d enter 9", getGoId())
 				break
 			}
-			log.Printf("%d enter 10", getGoId())
 		} else if b.state == RunningBackup {
-			log.Printf("%d enter 11", getGoId())
 			break
-		} else {
-			log.Printf("%d enter 12", getGoId())
 		}
 	}
 }
